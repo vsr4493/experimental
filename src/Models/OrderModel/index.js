a3bb9615712d11b227055c803c8972efb0fe9c4a
Avinash Agrawal <avinash8526@gmail.com>
2018-07-26 16:28:13 +0530

    Revert "Revert "Adding data models""

---
+++ src/Models/OrderModel/index.js
@@ -0,0 +1,4 @@
+export { default as InventoryPickups } from "./InventoryPickups";
+export { default as Invoices } from "./Invoices";
+export { default as SalesOrder } from "./SalesOrder";
+export { default as SalesOrderItems } from "./SalesOrderItems";
