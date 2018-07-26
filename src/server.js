dcd568e2b2328346c5c8a8c1d49437a3c857d008
Inderjit Singh <inderjit.singh@1mg.com>
2018-07-26 13:04:27 +0530

    Styled-components Server Stylesheets

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StaticRouter } from 'react-router-dom';
 import express from 'express';
 import { renderToString } from 'react-dom/server';
+import { ServerStyleSheet } from 'styled-components'
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
 const server = express();
@@ -11,11 +12,14 @@ server
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
   .get('/*', (req, res) => {
     const context = {};
+    const sheet = new ServerStyleSheet()
     const markup = renderToString(
-      <StaticRouter context={context} location={req.url}>
+      sheet.collectStyles(<StaticRouter context={context} location={req.url}>
         <App />
-      </StaticRouter>
+      </StaticRouter>)
     );
+    const styleTags = sheet.getStyleTags()
+
 
     if (context.url) {
       res.redirect(context.url);
@@ -28,6 +32,7 @@ server
         <meta charset="utf-8" />
         <title>Welcome to Razzle</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        ${styleTags}
         ${
           assets.client.css
             ? `<link rel="stylesheet" href="${assets.client.css}">`
