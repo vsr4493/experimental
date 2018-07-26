db84b148c7c302c00d71f0a90b7048f03d43871c
Vardhman Singh <vardhman.singh@1mg.com>
2018-07-26 16:42:58 +0530

    refactor(cleanup): refactor layout for site

--- razzle.config.js
+++ razzle.config.js
@@ -4,12 +4,14 @@ module.exports = {
   modify: (config, { target, dev }) => {
  
     config.resolve['alias'] = {
-      'components': path.resolve('./src/client/components'),
-      'pages': path.resolve('./src/client/pages'),
-      'layouts': path.resolve('./src/client/layouts'),
-      'hocs': path.resolve('./src/client/hocs'),
+      'components': path.resolve('./src/common/components'),
+      'pages': path.resolve('./src/common/pages'),
+      'layouts': path.resolve('./src/common/layouts'),
+      'hocs': path.resolve('./src/common/hocs'),
+      'common': path.resolve('./src/common'),
+      'client': path.resolve('./src/client'),
       'server': path.resolve('./src/server'),
-    }
+    };
     return config;
   }
 };
