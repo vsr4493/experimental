cd056f16d61648294cddc5dd48965eaa9e809e7b
Vardhman Singh <vardhman.singh@1mg.com>
2018-07-27 15:22:03 +0530

    disable add item form

--- src/common/pages/Admin/pages/AdminSupplierScheme/container.js
+++ src/common/pages/Admin/pages/AdminSupplierScheme/container.js
@@ -24,7 +24,7 @@ const mapStateToProps = ({ dataList }, ownProps) => ({
   data: selectors.getDataList(dataList),
   baseData: dataList.data,
   meta: dataList.meta,
-  allowsAdd: true,
+  allowsAdd: false,
   config,
   ...ownProps,
 });
