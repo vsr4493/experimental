93044ae42ab52253e6f7f3a96aa5b678d3d6ba8f
Vardhman Singh <vardhman.singh@1mg.com>
2018-07-27 10:40:44 +0530

    add fix for styling issues

--- src/common/App.js
+++ src/common/App.js
@@ -2,20 +2,12 @@ import React from 'react';
 import Route from 'react-router-dom/Route';
 import Redirect from 'react-router-dom/Redirect';
 import Switch from 'react-router-dom/Switch';
-import JssProvider from 'react-jss/lib/JssProvider';
-import { create } from 'jss';
-import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
 // Pages
 import Admin from 'pages/Admin';
 import Order from 'pages/Order';
 import Material from 'pages/Material';
 import Login from 'pages/Login';
 import NavBar from 'containers/NavBar';
-
-const generateClassName = createGenerateClassName();
-const jss = create(jssPreset());
-// We define a custom insertion point that JSS will look for injecting the styles in the DOM.
-jss.options.insertionPoint = 'jss-insertion-point';
 
 const isLoggedIn = () => {
 	if(typeof(window) !== 'undefined') {
@@ -30,28 +22,26 @@ const App = () => (
 		{isLoggedIn() &&
 			<NavBar />
 		}
-		<JssProvider jss={jss} generateClassName={generateClassName}>
-			<Switch>
-				{isLoggedIn() &&
-				  <Route path="/administration" component={Admin} />
-				}
-				{isLoggedIn() &&
-				  <Route path="/order-processing" component={Order} />
-				}
-				{isLoggedIn() &&
-				  <Route path="/material-management" component={Material} />
-				}
-				{!isLoggedIn() &&
-					<Route path="/login" component={Login} />
-				}
-				{isLoggedIn() &&
-					<Redirect to="/administration/product" />
-				}
-				{!isLoggedIn() &&
-					<Redirect to="/login" />
-				}
-			</Switch>
-		</JssProvider>
+		<Switch>
+			{isLoggedIn() &&
+			  <Route path="/administration" component={Admin} />
+			}
+			{isLoggedIn() &&
+			  <Route path="/order-processing" component={Order} />
+			}
+			{isLoggedIn() &&
+			  <Route path="/material-management" component={Material} />
+			}
+			{!isLoggedIn() &&
+				<Route path="/login" component={Login} />
+			}
+			{isLoggedIn() &&
+				<Redirect to="/administration/product" />
+			}
+			{!isLoggedIn() &&
+				<Redirect to="/login" />
+			}
+		</Switch>
 	</div>
 );
 
