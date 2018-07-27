c93bd1d7782fa7e1429c3f568bb4404d56397932
Rohit Jangid <rohitjangid@outlook.com>
2018-07-27 08:56:52 +0530

    Change default route and title

--- src/server.js
+++ src/server.js
@@ -39,7 +39,7 @@ server
     <head>
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta charset="utf-8" />
-        <title>Welcome to Razzle</title>
+        <title>Thanos - 1mg POS</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <style>
           body {
